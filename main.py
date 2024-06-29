9df8cafb4fcf6f17a54cf209717ee14f11d83c18
federpross78 <130210294+federpross78@users.noreply.github.com>
2024-06-29 11:55:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ TELEGRAM_TOKEN = 'TOKEN'
 TELEGRAM_CHANNEL = '@CHANNEL'
 
 coin_dict = {
-    "SOLUSDT": [144.22, 'Breakout'],
+    "SOLUSDT": [143.22, 'Breakout'],
     "WIFUSDT": [1.87, 'cross'],
 }
 
